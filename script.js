f333587a0a3448509bd68dc7b8dc18a11fc8b3aa
Alexander Chabin <nibach@ya.ru>
2022-07-13 00:03:02 +0500

    Вывод объектов на Яндекс.Карту

---
+++ script.js
@@ -0,0 +1,60 @@
+ymaps.ready(init);
+
+async function init() {
+    const myMap = new ymaps.Map('map', {
+        center: [56.838011, 60.597465],
+        zoom: 15,
+        controls: []
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+
+    const objectManager = new ymaps.ObjectManager({
+        clusterize: false,
+        gridSize: 32,
+        clusterDisableClickZoom: true
+    });
+
+    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+    myMap.geoObjects.add(objectManager);
+    myMap.controls.add('zoomControl');
+
+    const response = await fetch(`/api/map`);
+    const objects = await response.json();
+
+    console.log(objects);
+
+    const mapItems = objects.map(({
+        name,
+        description,
+        type,
+        coords,
+        street,
+        images,
+        date
+    }) => {
+        return {
+            type: 'Feature',
+            id: name,
+            geometry: {
+                type: 'Point',
+                coordinates: coords.split(', ')
+            },
+            properties: {
+                iconCaption: `${name} (${type})`,
+                balloonContent: `
+                        <h3>${name}</h3>
+
+                        <p>Улица: ${street}</p>
+                        <p>Тип: ${type}</p>
+                        <p>Описание: ${description}</p>
+                        <p>Дата: ${new Date(date).toLocaleDateString('ru')}</p>
+                        ${images?.map(x => `<br><img src="${x}" alt="" style="max-width: 100%" />`)}
+                        `,
+                hintContent: street
+            }
+        }
+    })
+    objectManager.add(mapItems);
+}
