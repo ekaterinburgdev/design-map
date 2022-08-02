8f5724d0e5ca0a1f123c6f370dc9b054c74dc9c7
Alexander Chabin <nibach@ya.ru>
2022-08-03 04:22:44 +0500

    Добавил типы иконок, обновил форматирование

--- script.js
+++ script.js
@@ -1,60 +1,77 @@
 ymaps.ready(init);
 
 async function init() {
-    const myMap = new ymaps.Map('map', {
-        center: [56.838011, 60.597465],
-        zoom: 15,
-        controls: []
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
+	const myMap = new ymaps.Map('map', {
+		center: [56.838011, 60.597465],
+		zoom: 15,
+		controls: []
+	});
 
-    const objectManager = new ymaps.ObjectManager({
-        clusterize: false,
-        gridSize: 32,
-        clusterDisableClickZoom: true
-    });
+	const objectManager = new ymaps.ObjectManager({
+		clusterize: false,
+		gridSize: 64,
+		clusterDisableClickZoom: true
+	});
 
-    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
-    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
-    myMap.geoObjects.add(objectManager);
-    myMap.controls.add('zoomControl');
+	objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+	objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+	myMap.geoObjects.add(objectManager);
 
-    const response = await fetch(`/api/map`);
-    const objects = await response.json();
+	const response = await fetch(`/api/map`);
+	const objects = await response.json();
 
-    console.debug(objects);
+	const types = {
+		'Логотип': 'orange',
+		'Навигационные стелы': 'yellow',
+		'Таблички ОКН': 'olive',
+		'Таблички ЧО': 'gray',
+		'Обыные адресные таблички': 'violet',
+		'Фризы остановок': 'pink',
+		'Светофор': 'lightBlue',
+		'Исторические адресные таблички': 'black'
+	}
 
-    const mapItems = objects.map(({
-        name,
-        description,
-        type,
-        coords,
-        street,
-        images,
-        date
-    }, i) => {
-        return {
-            type: 'Feature',
-            id: name + i,
-            geometry: {
-                type: 'Point',
-                coordinates: coords.split(', ')
-            },
-            properties: {
-                iconCaption: `${name} (${type})`,
-                balloonContent: `
-                        <h3>${name}</h3>
-
-                        <p>Улица: ${street}</p>
-                        <p>Тип: ${type}</p>
-                        <p>Описание: ${description}</p>
-                        <p>Дата: ${new Date(date).toLocaleDateString('ru')}</p>
-                        ${images?.map(x => `<br><img src="${x}" alt="" style="max-width: 100%" />`)}
-                        `,
-                hintContent: street
-            }
-        }
-    })
-    objectManager.add(mapItems);
+	const mapItems = objects.map(({
+		name,
+		description,
+		type,
+		coords,
+		street,
+		images,
+		date
+	}, i) => {
+		return {
+			type: 'Feature',
+			id: name + i,
+			geometry: {
+				type: 'Point',
+				coordinates: coords.split(', ')
+			},
+			options: {
+				preset: `islands#${types[type]}CircleDotIcon`
+			},
+			properties: {
+				balloonContent: `
+					<div class="balloon">
+						<p class="balloon__name">${name}</p>
+						${type ?
+						`<p class="balloon__type">${type}</p>` : ''}
+						${street ?
+						`<address class="balloon__address">${street}</address>` : ''}
+						${description ?
+						`<p class="balloon__description">${description}</p>` : ''}
+						${date ?
+						`<p class="balloon__date">${new Date(date).toLocaleDateString('ru')}</p>` : ''}
+						${images?.map(x => `<br><img src="${x}" alt="" style="max-width: 100%" />`).join('<br />')}
+					</div>
+					`,
+				hintContent: `
+					<b>${name}</b>
+					<br />
+					${street}
+				`
+			}
+		}
+	})
+	objectManager.add(mapItems);
 }
