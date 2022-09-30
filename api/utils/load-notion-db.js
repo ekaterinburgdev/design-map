2b37956619f8df20c301c83645d88704f14acb52
Alexander Chabin <nibach@ya.ru>
2022-09-30 18:08:24 +0500

    Loading all ekaterinburg.design objects

--- api/utils/load-notion-db.js
+++ api/utils/load-notion-db.js
@@ -1,16 +1,36 @@
 import fetch from 'node-fetch';
 import extractNotionValue from './extract-notion-value.js';
 
-async function loadNotionDB(notionDB, notionToken, notionVersion = '2022-02-22') {
-    const response = await fetch(`https://api.notion.com/v1/databases/${notionDB}/query`, {
+async function loadNotionDB(notionDB, notionToken, notionVersion = '2022-06-28') {
+    const requestHeaders = {
+        'Authorization': `Bearer ${notionToken}`,
+        'Notion-Version': notionVersion,
+        "Content-Type": "application/json"
+    }
+
+    let results = [];
+
+    let response = await fetch(`https://api.notion.com/v1/databases/${notionDB}/query`, {
         method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${notionToken}`,
-            'Notion-Version': notionVersion
-        }
+        headers: requestHeaders
     });
 
-    const { results } = await response.json();
+    let data = await response.json();
+
+    results = [...data.results]
+
+    while (data.next_cursor) {
+        response = await fetch(`https://api.notion.com/v1/databases/${notionDB}/query`, {
+            method: 'POST',
+            headers: requestHeaders,
+            body: JSON.stringify({
+                start_cursor: data.next_cursor
+            })
+        });
+    
+        data = await response.json();
+        results = [...results, ...data.results]
+    }
 
     return results
         .map(({ properties }) => properties)
