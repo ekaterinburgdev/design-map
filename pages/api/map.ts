e02c90f66a4838584c519bc80a7b1517edb2270d
Alexander Chabin <nibach@ya.ru>
2022-11-15 09:46:55 +0500

    Add leaflet `LatLngExpression` type for coords

--- pages/api/map.ts
+++ pages/api/map.ts
@@ -4,13 +4,17 @@ import loadNotionDB from './utils/load-notion-db';
 
 const { NOTION_TOKEN, NOTION_DATABASE } = process.env;
 
+function getCoords(coords : string) {
+    return coords.split(', ').map((x) => Number(x));
+}
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     const mapItemsDB = await loadNotionDB(NOTION_DATABASE, NOTION_TOKEN);
     const mapItems = mapItemsDB.map((item) => ({
         name: item.Name,
         type: item.Type,
         description: item.Description,
-        coords: item.Coords,
+        coords: getCoords(item.Coords),
         street: item.Street,
         images: item.Images,
         date: item.Date,
