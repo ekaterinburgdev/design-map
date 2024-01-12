11e4a8aebaca4f8a0940825afab33a514524b8cd
Alexander Chabin <nibach@ya.ru>
2024-01-12 09:11:28 +0500

    Remove Next.js API

--- pages/api/map.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import placemarks from 'public/notion-static/placemarks.json';
-
-export default function handler(_: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json(placemarks);
-}
