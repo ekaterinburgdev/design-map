e1a07c1c01e785268875a1f1a40e4053e76a11da
Sasha Chabin <nibach@ya.ru>
2024-11-24 14:40:19 +0500

    Update Next.js & fix warnings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
 import { Map } from 'components/Map';
 import { Footer } from 'components/Footer/Footer';
 import { MapContextProvider } from 'components/Map/providers/MapProvider';
 import placemarks from 'public/notion-static/placemarks.json';
 
+const siteTitle = 'Карта объектов Дизайн-кода Екатеринбурга';
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
             <MapContextProvider>
                 <Map placemarksData={placemarks} />
             </MapContextProvider>
